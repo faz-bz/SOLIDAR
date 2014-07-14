267aba3443bfdd57446c4c88e34497ca9171492e
Your Name <you@example.com>
2014-07-14 13:07:44 +0200

    changed RPC and  P2P ports

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 18638 : 8638;
+    return GetBoolArg("-testnet", false) ? 45888 : 55888;
 }
 
 Object JSONRPCError(int code, const string& message)
