8f6be9c6de0c2bcdae15cf1a2bed321744a65a02
Rikski <hendrik-richter@freenet.de>
2016-02-12 15:18:37 +0100

    Switch to CBlockTreeDB

--- src/db.h
+++ src/db.h
@@ -307,18 +307,7 @@ public:
 };
 
 
-// POS
-/** Access to the transaction database (blkindex.dat) */
-class CTxDB : public CDB
-{
-public:
-    CTxDB(const char* pszMode="r+") : CDB("blkindex.dat", pszMode) { }
-private:
-    CTxDB(const CTxDB&);
-    void operator=(const CTxDB&);
-public:
-    bool ReadTxIndex(uint256 hash, CTxIndex& txindex);
-};
+
 
 
 
