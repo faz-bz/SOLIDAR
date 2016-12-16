460ab9b0301aebc54d6d6bb54918b574f1e661ab
Hendrik Richter <info@einc-ev.org>
2016-12-16 20:48:40 +0100

    include new chainID for merge mining

--- src/main.cpp
+++ src/main.cpp
@@ -2308,6 +2308,12 @@ int GetAuxPowStartBlock()
 
 int GetOurChainID()
 {
+    return 0x5588;
+}
+
+// solidar will accept the old and the new ChainID
+int GetOldChainID()
+{
     return 0x0011;
 }
 
@@ -2320,7 +2326,7 @@ bool CBlockHeader::CheckProofOfWork(int nHeight) const
         // - parent block must not have the same chain ID (see CAuxPow::Check)
         // - index of this chain in chain merkle tree must be pre-determined (see CAuxPow::Check)
 
-        if (!fTestNet && nHeight != INT_MAX && GetChainID() != GetOurChainID())
+        if (!fTestNet && nHeight != INT_MAX && !(GetChainID() != GetOurChainID() || GetChainID() != GetOldChainID()))
             return error("CheckProofOfWork() : block does not have our chain ID");
 		
         if (auxpow.get() != NULL)
